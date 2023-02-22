a5c71e9d8c496ee6727f98d244feb1d766a979fe
jufrin01 <125728570+jufrin01@users.noreply.github.com>
2023-02-22 20:21:47 +0700

    Jurin

---
+++ Jufrin/OperatorArimatika.java
@@ -0,0 +1,11 @@
+public class OperatorArimatika {
+    public static void main(String[] args) {
+        int ver1 =10;
+        int ver = 20;
+        int hasil;
+
+        hasil = ver1+ver;
+        System.out.println(hasil);
+
+    }
+}
